2cac5dc7cf19420f80be5754a4c1f76e0c3d3345
wangli <wanglijtm@gmail.com>
2023-07-06 22:54:46 +0200

    ajout de modeDark

---
+++ fonction.js
@@ -0,0 +1,42 @@
+/*----------Mode jourNuit--------*/
+
+
+
+        const toggle_btn = document.getElementById('theme-btn');
+
+        const service = document.getElementsByClassName('service');
+
+const body = document.getElementsByTagName('body')[0];
+
+console.log('toggle_btn',toggle_btn);
+
+const dark_theme_class = 'dark';
+
+toggle_btn.addEventListener('click', function() {
+
+ if (body.classList.contains(dark_theme_class)) {
+ body.classList.remove(dark_theme_class);
+
+}
+
+ else {
+
+ body.classList.add(dark_theme_class);
+
+};
+
+
+if (toggle_btn.innerText ==='🌙') {
+  toggle_btn.innerText= '🌝';
+  service.add.classList('serviceChangeColor');
+
+}else{
+  toggle_btn.innerText= '🌙';
+  service.add.classList('serviceChangeColor');
+}
+
+
+
+
+
+});
